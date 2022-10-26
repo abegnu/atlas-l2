8bfc9c8994636461d3b6899329f92fec8506971a
abegnu <102627853+abegnu@users.noreply.github.com>
2022-10-26 17:18:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h2>Pushed from local repo</h2>
+      <h2>Changes from local repo</h2>
     </div>
   )
 }
